b64b2f5a67fe0acb8d4136d0a6285ed24543b1e4
程鑫 <chengxin@300.cn>
2023-08-10 11:19:58 +0800

    Initial Commit

---
+++ C2/C2App.swift
@@ -0,0 +1,17 @@
+//
+//  C2App.swift
+//  C2
+//
+//  Created by chengxin on 2023/8/10.
+//
+
+import SwiftUI
+
+@main
+struct C2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ C2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  C2
+//
+//  Created by chengxin on 2023/8/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
